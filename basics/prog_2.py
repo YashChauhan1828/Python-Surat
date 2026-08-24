e6ef83639e954a23b8781f1deabdcfedad8cba04
YashChauhan1828 <chauhanyash1804@gmail.com>
2026-08-24 17:38:53 +0530

    first commit

---
+++ basics/prog_2.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+
+print("Hello " + name)
+print(type(name))
+
+age = int(input("Enter your age: "))
+print("Your age is: ", age)
+
+
+age = float(int(input("Enter your age: ")))
+print("Your age is: ", age)
