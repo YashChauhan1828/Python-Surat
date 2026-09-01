6dd99aa0e31913a2663827801ac7ccc4058b4e4c
YashChauhan1828 <chauhanyash1804@gmail.com>
2026-09-01 17:02:55 +0530

    Math1 Demo

---
+++ Math/math1demo.py
@@ -0,0 +1,28 @@
+import math
+
+a = 15
+# Square root
+# print("Square root : ",math.sqrt(a))
+
+# power
+# print("Power : ",math.pow(a,6))
+
+# Factorial
+# print("Factorial : ",math.factorial(15))
+
+# Ceil value
+# print("Ceil value for positive: ",math.ceil(3.0000000001))
+# print("ceil value for negative: ",math.ceil(-4.358))
+
+
+# Floor Value
+# print("Floor Value: ",math.floor(3.222222))
+# print("floor value for negative: ",math.floor(-6.821))
+
+# absolute value
+# print("Absolute Value: ",math.fabs(-7.842))
+# print("Absolute Value: ",math.fabs(7.842))
+
+# #logithmic value
+# print("Logarithmic value: ",math.log(100))
+# print("Logarithmic value: ",math.log(100,10)) 
