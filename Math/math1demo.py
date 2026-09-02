5ed0db1674d7503f5186dc1ccddd0c4bb646d673
YashChauhan1828 <chauhanyash1804@gmail.com>
2026-09-02 22:47:38 +0530

    Math advance

--- Math/math1demo.py
+++ Math/math1demo.py
@@ -24,31 +24,40 @@ a = 15
 # print("Absolute Value: ",math.fabs(7.842))
 
 #logithmic value
-print("Logarithmic value: ",math.log(100)) 
-print("Logarithmic value: ",math.log(100,10)) 
+# print("Logarithmic value: ",math.log(100)) 
+# print("Logarithmic value: ",math.log(100,10)) 
 
-# Exponential Value
-print("Exponential Value: ",math.exp(7))
+# # Exponential Value
+# print("Exponential Value: ",math.exp(7))
 
-# GCD/HCF
-print("GCD is: ",math.gcd(12,15))
-print("GCD is: ",math.gcd(19,15))
+# # GCD/HCF
+# print("GCD is: ",math.gcd(12,15))
+# print("GCD is: ",math.gcd(19,15))
 
-# LCM
-print("LCM is : ",math.lcm(12,15))
+# # LCM
+# print("LCM is : ",math.lcm(12,15))
 
-# # Trigonometric Functions
+# # # Trigonometric Functions
 
-print("Sine Value: ",math.sin(math.radians(0)))
-print("Cosine value: ",math.cos(math.radians(0)))
-print("Tangent Value: ",math.tan(math.radians(90)))
-print("Cosecant Value : ",1 / (math.sin(math.radians(30))))
-print("Secant Value : ",1 / (math.cos(math.radians(60))))
-print("Cotangent Value : ",1 / (math.tan(math.radians(45))))
+# print("Sine Value: ",math.sin(math.radians(0)))
+# print("Cosine value: ",math.cos(math.radians(0)))
+# print("Tangent Value: ",math.tan(math.radians(90)))
+# print("Cosecant Value : ",1 / (math.sin(math.radians(30))))
+# print("Secant Value : ",1 / (math.cos(math.radians(60))))
+# print("Cotangent Value : ",1 / (math.tan(math.radians(45))))
 
-# # constant
-print("Value of pi: ",math.pi)
-print("Value of e: ",math.e)
+# # # constant
+# print("Value of pi: ",math.pi)
+# print("Value of e: ",math.e)
 
 # modf
 print(math.modf(5.75))
+
+# permutations
+print("Permutations: ",math.perm(5,3))
+
+# combinations
+print("Combinations: ",math.comb(5,3))
+
+# remainder
+# print("Remainder: ",math.remainder(3,5))
