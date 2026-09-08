681cf44a9456694c16e8c4e454b68a760e95f242
YashChauhan1828 <chauhanyash1804@gmail.com>
2026-09-08 17:14:14 +0530

    For loop

---
+++ loops/forloop1.py
@@ -0,0 +1,12 @@
+# for i in range(10):
+#     print(i , end=" ")
+
+# for i in range(1,30):
+#     print(i , end=" ")
+
+# for i in range(1,30,3):
+#     print(i , end=" ")
+
+
+for i in range(7,0,-1):
+    print(i)
