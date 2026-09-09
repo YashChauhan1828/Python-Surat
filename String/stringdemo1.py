b48b8667d0d798ec21f745927b88bbf00b38f8f5
YashChauhan1828 <chauhanyash1804@gmail.com>
2026-09-09 15:33:29 +0530

    String 1

---
+++ String/stringdemo1.py
@@ -0,0 +1,22 @@
+# name = "Yash"
+# print(type(name) , name)
+
+# singlevariable = "x"
+# print(type(singlevariable))
+
+# numeric = "1212121"
+# print(type(numeric))
+
+alphanumeric = "Yash@1804"
+print(type(alphanumeric))
+# alphanumeric[6] = 6
+# print(alphanumeric[6]) # Left to right accessing starts with 0 index
+print(alphanumeric[-2])  # Right to left accessing starts with -1 index
+
+
+# empty = " "
+# # print(type(empty))
+# print(empty[0])
+# empty[0] = "a"
+# print(empty)
+
