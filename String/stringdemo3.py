ade4c31197f09740a5fb1976ec0d4260437b6450
YashChauhan1828 <chauhanyash1804@gmail.com>
2026-09-10 16:29:37 +0530

    String 3

---
+++ String/stringdemo3.py
@@ -0,0 +1,16 @@
+name = "Yash"
+
+# 1) Length Function
+print(len(name))
+print(len("121212121"))
+
+# 2) Ordinal Function
+
+print(ord("a")) # Returns the ascii value of the character.
+
+# 3) character Function
+
+print(chr(122)) # Returns the character of the ascii value.
+
+
+# A to Z print 
